b01fe22e20ae3bc8de95a904b061e0c54dc796d2
Sushanta Shrestha <142810781+sshres10@users.noreply.github.com>
2024-04-15 13:15:00 -0400

    app.py

--- app/app.py
+++ app/app.py
@@ -24,17 +24,17 @@ with ui.sidebar(title="Filter controls"):
     ui.h6("Links")
     ui.a(
         "GitHub Source",
-        href="https://github.com/denisecase/cintel-07-tdash",
+        href="https://github.com/sshres10/cintel-07-tdash",
         target="_blank",
     )
     ui.a(
         "GitHub App",
-        href="https://denisecase.github.io/cintel-07-tdash/",
+        href="https://github.com/sshres10/cintel-07-tdash/blob/main/app/app.py",
         target="_blank",
     )
     ui.a(
         "GitHub Issues",
-        href="https://github.com/denisecase/cintel-07-tdash/issues",
+        href="https://github.com/sshres10/cintel-07-tdash/issues",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
@@ -45,7 +45,7 @@ with ui.sidebar(title="Filter controls"):
     )
     ui.a(
         "See also",
-        href="https://github.com/denisecase/pyshiny-penguins-dashboard-express",
+        href="https://sshres10.github.io/cintel-07-tdash/",
         target="_blank",
     )
 
